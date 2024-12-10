b51d82d4e2470a7c0082fa521e3244b9755176a1
Guiorgy <Guiorgy123@gmail.com>
2024-12-10 16:30:07 +0400

    use exact version tag when pulling

--- dockerhub-build-and-push.ps1
+++ dockerhub-build-and-push.ps1
@@ -8,7 +8,7 @@ echo "Building images for $VERSION"
 
 # docker buildx create --name mosquitto-hc
 # docker buildx use mosquitto-hc
-docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm64/v8 -t "guiorgy/mosquitto-hc:latest" --build-arg TAG="latest" --push .
+docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm64/v8 -t "guiorgy/mosquitto-hc:latest" --build-arg TAG="$VERSION" --push .
 docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm64/v8 -t "guiorgy/mosquitto-hc:$VERSION" --build-arg TAG="$VERSION" --push .
-docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm64/v8 -t "guiorgy/mosquitto-hc:openssl" --build-arg TAG="openssl" --push .
+docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm64/v8 -t "guiorgy/mosquitto-hc:openssl" --build-arg TAG="$VERSION-openssl" --push .
 docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm64/v8 -t "guiorgy/mosquitto-hc:$VERSION-openssl" --build-arg TAG="$VERSION-openssl" --push .
