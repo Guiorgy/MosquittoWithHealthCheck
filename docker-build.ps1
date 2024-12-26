f857c28a4b6a350bb9d70c051b14400c25c22133
Guiorgy <Guiorgy123@gmail.com>
2024-12-26 20:17:00 +0400

    made the PowerShell script save image as tar.gz

--- docker-build.ps1
+++ docker-build.ps1
@@ -11,4 +11,10 @@ if (!$TAG) {
 echo "Building image for $TAG"
 
 docker build --build-arg TAG="$TAG" -t "mosquitto-hc:$TAG" -f Dockerfile .
-docker save -o ".\mosquitto-hc-$TAG.tar" "mosquitto-hc:$TAG"
+
+if ($PSVersionTable.PSVersion.Major -ge 7) {
+  docker save "mosquitto-hc:$TAG" | gzip --best --stdout --verbose > "./mosquitto-hc-$TAG.tar.gz"
+} else {
+  # PowerShell 5 doesn't handle byte streams properly resulting in a corrupted archive
+  cmd /c "docker save mosquitto-hc:$TAG | gzip --best --stdout --verbose > ./mosquitto-hc-$TAG.tar.gz"
+}
