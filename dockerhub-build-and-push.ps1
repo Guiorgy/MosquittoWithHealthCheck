20b694972e27e4f2612e886a1ca8b71a7fd6e3d0
Guiorgy <Guiorgy123@gmail.com>
2026-02-16 15:12:33 +0400

    updated tags following changes in 2.1

--- dockerhub-build-and-push.ps1
+++ dockerhub-build-and-push.ps1
@@ -16,11 +16,8 @@ function Build-Version {
     $Version
   )
 
-  Write-Host "Building $Version based on $Version"
-  docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm64/v8 -t "guiorgy/mosquitto-hc:$Version" --build-arg TAG="$Version" --push .
-
-  Write-Host "Building $Version-openssl based on $Version-openssl"
-  docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm64/v8 -t "guiorgy/mosquitto-hc:$Version-openssl" --build-arg TAG="$Version-openssl" --push .
+  Write-Host "Building $Version-alpine based on $Version-alpine"
+  docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm64/v8 -t "guiorgy/mosquitto-hc:$Version-alpine" --build-arg TAG="$Version-alpine" --push .
 }
 
 # Build the exact version tag as well as latest, major.minor and major tags
@@ -34,11 +31,8 @@ function Build-All {
   $MajorMinor = "$($Version.Major).$($Version.Minor)"
   $Major = "$($Version.Major)"
 
-  Write-Host "Building latest, $Version, $MajorMinor and $Major based on $Version"
-  docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm64/v8 -t "guiorgy/mosquitto-hc:latest" -t "guiorgy/mosquitto-hc:$Version" -t "guiorgy/mosquitto-hc:$MajorMinor" -t "guiorgy/mosquitto-hc:$Major" --build-arg TAG="$Version" --push .
-
-  Write-Host "Building openssl, $Version-openssl, $MajorMinor-openssl and $Major-openssl based on $Version-openssl"
-  docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm64/v8 -t "guiorgy/mosquitto-hc:openssl" -t "guiorgy/mosquitto-hc:$Version-openssl" -t "guiorgy/mosquitto-hc:$MajorMinor-openssl" -t "guiorgy/mosquitto-hc:$Major-openssl" --build-arg TAG="$Version-openssl" --push .
+  Write-Host "Building latest, alpine, $Version-alpine, $MajorMinor-alpine and $Major based on $Version-alpine"
+  docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm64/v8 -t "guiorgy/mosquitto-hc:latest" -t "guiorgy/mosquitto-hc:alpine" -t "guiorgy/mosquitto-hc:$Version-alpine" -t "guiorgy/mosquitto-hc:$MajorMinor-alpine" -t "guiorgy/mosquitto-hc:$Major" --build-arg TAG="$Version-alpine" --push .
 }
 
 # Separate the versions, convert them to [version] objects, and sort them
